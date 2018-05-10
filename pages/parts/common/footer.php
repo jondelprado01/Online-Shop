679c06919f821f3a285f3cda673ac18d872e3af8
LincolnClayMF3 <38089819+LincolnClayMF3@users.noreply.github.com>
2018-05-10 17:03:00 +0800

    Initial commit

---
+++ pages/parts/common/footer.php
@@ -0,0 +1,106 @@
+<div class="col-md-12 download-app-box text-center">
+
+    <span class="glyphicon glyphicon-download-alt"></span>Download Our Android App and Get 10% additional Off on all Products . <a href="#" class="btn btn-danger btn-lg">DOWNLOAD  NOW</a>
+
+</div>
+
+<!--Footer -->
+<div class="col-md-12 footer-box">
+
+    <div class="row small-box ">
+        <strong>Mobiles :</strong> <a href="#">samsung</a> |  <a href="#">Sony</a> | <a href="#">Microx</a> |
+        <a href="#">samsung</a> |  <a href="#">Sony</a> | <a href="#">Microx</a> |<a href="#">samsung</a> |
+          <a href="#">Sony</a> | <a href="#">Microx</a> |<a href="#">samsung</a> |  <a href="#">Sony</a> |
+        <a href="#">Microx</a> |<a href="#">samsung</a> |  <a href="#">Sony</a> | <a href="#">Microx</a> |
+        <a href="#">samsung</a> |  <a href="#">Sony</a> | <a href="#">Microx</a> |<a href="#">samsung</a> |
+        <a href="#">Sony</a> | <a href="#">Microx</a> | view all items
+
+    </div>
+    <div class="row small-box ">
+        <strong>Laptops :</strong> <a href="#">samsung</a> |  <a href="#">Sony</a> | <a href="#">Microx Laptops</a> |
+        <a href="#">samsung</a> |  <a href="#">Sony</a> | <a href="#">Microx</a> |<a href="#">samsung</a> |
+          <a href="#">Sony Laptops</a> | <a href="#">Microx</a> |<a href="#">samsung</a> |  <a href="#">Sony</a> |
+        <a href="#">Microx</a> |<a href="#">samsung</a> |  <a href="#">Sony</a> | <a href="#">Microx</a> |
+        <a href="#">samsung</a> |  <a href="#">Sony</a> | <a href="#">Microx</a> |<a href="#">samsung</a> |
+        <a href="#">Sony</a> | <a href="#">Microx</a> | view all items
+    </div>
+    <div class="row small-box ">
+        <strong>Tablets : </strong><a href="#">samsung</a> |  <a href="#">Sony Tablets</a> | <a href="#">Microx</a> |
+        <a href="#">samsung </a>|  <a href="#">Sony</a> | <a href="#">Microx</a> |<a href="#">samsung</a> |
+          <a href="#">Sony</a> | <a href="#">Microx</a> |<a href="#">samsung Tablets</a> |  <a href="#">Sony</a> |
+        <a href="#">Microx</a> |<a href="#">samsung Tablets</a> |  <a href="#">Sony</a> | <a href="#">Microx</a> |
+        <a href="#">samsung</a> |  <a href="#">Sony</a> | <a href="#">Microx</a> |<a href="#">samsung</a> |
+        <a href="#">Sony</a> | <a href="#">Microx Tablets</a> | view all items
+
+    </div>
+    <div class="row small-box pad-botom ">
+        <strong>Computers :</strong> <a href="#">samsung</a> |  <a href="#">Sony</a> | <a href="#">Microx</a> |
+        <a href="#">samsung Computers</a> |  <a href="#">Sony</a> | <a href="#">Microx</a> |<a href="#">samsung</a> |
+          <a href="#">Sony</a> | <a href="#">Microx</a> |<a href="#">samsung</a> |  <a href="#">Sony</a> |
+        <a href="#">Microx Computers</a> |<a href="#">samsung Computers</a> |  <a href="#">Sony</a> | <a href="#">Microx</a> |
+        <a href="#">samsung</a> |  <a href="#">Sony</a> | <a href="#">Microx Computers</a> |<a href="#">samsung</a> |
+        <a href="#">Sony</a> | <a href="#">Microx</a> | view all items
+
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <strong>Send a Quick Query </strong>
+            <hr>
+            <form>
+                <div class="row">
+                    <div class="col-md-6 col-sm-6">
+                        <div class="form-group">
+                            <input type="text" class="form-control" required="required" placeholder="Name">
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-6">
+                        <div class="form-group">
+                            <input type="text" class="form-control" required="required" placeholder="Email address">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12">
+                        <div class="form-group">
+                            <textarea name="message" id="message" required="required" class="form-control" rows="3" placeholder="Message"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Submit Request</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+        <div class="col-md-4">
+            <strong>Our Location</strong>
+            <hr>
+            <p>
+                 234, New york Street,<br />
+                                Just Location, USA<br />
+                Call: +09-456-567-890<br>
+                Email: info@yourdomain.com<br>
+            </p>
+
+            2014 www.yourdomain.com | All Right Reserved
+        </div>
+        <div class="col-md-4 social-box">
+            <strong>We are Social </strong>
+            <hr>
+            <a href="#"><i class="fa fa-facebook-square fa-3x "></i></a>
+            <a href="#"><i class="fa fa-twitter-square fa-3x "></i></a>
+            <a href="#"><i class="fa fa-google-plus-square fa-3x c"></i></a>
+            <a href="#"><i class="fa fa-linkedin-square fa-3x "></i></a>
+            <a href="#"><i class="fa fa-pinterest-square fa-3x "></i></a>
+            <p>
+                Like our Facebook Page pconlineshop. <br>Follow our Tweets at pconlineshop. <br>
+                Email us at pconlineshop@gmail.com. <br>
+            </p>
+        </div>
+    </div>
+    <hr>
+</div>
+<!-- /.col -->
+<div class="col-md-12 end-box ">
+    &copy; 2018 | &nbsp; All Rights Reserved | &nbsp; www.pconlineshop.com | &nbsp; 24x7 support | &nbsp; Email us: pconlineshop.gmail.com
+</div>
---
+++ pages/parts/common/head.php
@@ -0,0 +1,17 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Sample Web System</title>
+<!-- Bootstrap core CSS -->
+<link href="assets/css/bootstrap.css" rel="stylesheet">
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<!-- Fontawesome core CSS -->
+<link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+<!--GOOGLE FONT -->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+<!--Slide Show Css -->
+<link href="assets/ItemSlider/css/main-style.css" rel="stylesheet" />
+<!-- custom CSS here -->
+<link href="assets/css/style.css" rel="stylesheet" />
