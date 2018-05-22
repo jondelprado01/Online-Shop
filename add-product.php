1a2bc8a7fdaeb8fbcfe5927cdeb90697ac3a5f8b
LincolnClayMF3 <38089819+LincolnClayMF3@users.noreply.github.com>
2018-05-22 22:49:57 +0800

    Main
    Testing of Product adding feature

---
+++ add-product.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <form class="" method="post">
+
+      <input type="text" name="name" placeholder="Product Name">
+      <input type="text" name="price" placeholder="Product Price">
+      <input type="file" name="img-path">
+      <input type="submit" name="btn-submit" value="Save">
+
+    </form>
+
+
+
+  </body>
+</html>
+
+<?php
+
+
+
+ ?>
