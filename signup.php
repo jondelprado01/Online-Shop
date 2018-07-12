d1aa752ba661ca238184272690ff156698132ff8
Elijah <32192896+froztbyte78@users.noreply.github.com>
2018-07-12 13:01:40 +0800

    head.php fix + signup.php initial edit

--- signup.php
+++ signup.php
@@ -1,72 +1,56 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="">
-  <meta name="author" content="">
+  <?php
 
-  <!-- <link href="assets/css/bootstrap.css" rel="stylesheet"> -->
-  <link href="assets/css/bootstrap-4.0.0.min.css" rel="stylesheet">
-  <link href="assets/css/login-style.css" rel="stylesheet">
-  <link href="assets/css/style.css" rel="stylesheet">
-  <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
-  <link href="assets/css/alertify.min.css" rel="stylesheet"/>
-  <link href="assets/css/default.min.css" rel="stylesheet"/>
-  <link href="assets/css/semantic.min.css" rel="stylesheet"/>
+    require 'pages/parts/common/head.php';
 
-  <script src="assets/js/jquery.min.js"></script>
-  <!-- <script src="assets/js/bootstrap.min.js"></script> -->
-  <script src="assets/js/bootstrap-4.0.0.min.js"></script>
-  <script src="assets/js/alertify.min.js"></script>
-  <script src="assets/js/popper.js"></script>
-
-  <title>Online Shop</title>
+  ?>
 </head>
 <body>
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-dark my-0 py-0 rounded-bottom">
-    <!-- BRAND AND COLLAPSE BUTTON -->
-    <a href="index.php " class="navbar-brand text-light">Online Shop</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+<?php
+
+  require 'pages/parts/common/navigation.php';
+
+ ?>
+
+<div class="row">
+  <div class="col-2">
     <!--  -->
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <!-- SEARCH BAR -->
-      <!-- <form class="input-group form-inline input-group-sm w-50 my-2 mx-sm-auto">
-        <input class="form-control mr-sm-0" type="search" placeholder="" aria-label="Search">
-          <div class="input-group-append">
-            <button class="btn btn-outline-white my-2 my-sm-0" type="submit">Search</button>
+  </div>
+  <div class="col container bg-secondary rounded p-3">
+    <div class="container bg-light p-3">
+      <p class="text-center h3">Create an <b>Online Shop</b> account to start shopping online!</p>
+      <br>
+      <br>
+      <p class="text-left text-muted px-3"> Personal Information</p>
+      <div class="row px-3">
+        <div class="col-md-4">
+          <div class="form-group">
+            <label for="first-name">First Name <i class="fa fa-asterisk"></i></label>
+            <input type="text" class="form-control" name="firstname" placeholder="First Name" required>
           </div>
-      </form> -->
-      <!--  -->
-      <!-- NAVIGATION -->
-      <ul class="navbar-nav ml-auto">
-
-          <li class="nav-item active">
-            <a href="login.php" class="nav-link text-light">Login</a>
-          </li>
-
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Contact Us
-            </a>
-            <div class="dropdown-menu dropdown-menu-right bg-light" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">0912 345 6789</a>
-              <a class="dropdown-item" href="#">onlineshop.com</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Address:</a>
-              <a class="dropdown-item" href="#">Metro Manila</a>
-              <a class="dropdown-item" href="#">NCR, Philippines</a>
-            </div>
-          </li>
-
-      </ul>
-      <!--  -->
+        </div>
+        <div class="col-md-4">
+          <div class="form-group">
+            <label for"last-name">Last Name <i class="fa fa-asterisk"></i></label>
+            <input type="text" class="form-control" name="lastname" placeholder="Last Name" required>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="form-group">
+            <label for"middle-name">Middle Name</label>
+            <input type="text" class="form-control" name="middlename" placeholder="Middle Name" required>
+          </div>
+        </div>
+      </div>
     </div>
-  </nav><br>
-
+  </div>
+  <div class="col-2">
+    <!--  -->
+  </div>
+</div>
 
 <div class="center">
 
